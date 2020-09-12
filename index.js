e3e266ce7e1079ac75a916fefde5025201cd0259
codego <joacoditoma@gmail.com>
2020-09-12 11:16:03 -0300

    prueba2

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('asd');
+const branch = 'refs/heads/COR-1830/Nombre-de-la-branch';
+const corStr = branch.split('COR-');
+const separado = corStr[1].split('/');
+console.log(separado[0]);
